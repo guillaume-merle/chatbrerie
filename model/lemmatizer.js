bb9f45881fc618c558a7c60808b3d4f3aacd2ea0
Marc Demoustier <marc.demoustier@epita.fr>
2021-05-09 23:19:11 +0200

    read word-lists.txt and convert it in an array of word

--- model/lemmatizer.js
+++ model/lemmatizer.js
@@ -1,29 +1,35 @@
-var NlpjsTFr = require('nlp-js-tools-french');
+function lemmatizeInput(input) {
+  var NlpjsTFr = require('nlp-js-tools-french');
 
-const fs = require('fs');
-
-var config = {
+  var config = {
     tagTypes: ["adj", "adv", "art", "con", "nom", "ono", "pre", "ver", "pro"],
     strictness: false,
     minimumLength: 3,
     debug: false
-};
+  };
 
-test = "Bonjour, des comment allez-vous ?";
+  var nlpToolsFr = new NlpjsTFr(input, config);
+  var outputList = [];
 
-var nlpToolsFr = new NlpjsTFr(test, config);
-var output_list = [];
+  var dict = Array.from(nlpToolsFr.lemmatizer())
 
-var dict = Array.from(nlpToolsFr.lemmatizer())
+  // Fill the output_list with the first occurence of each lemmatize word
+  var lastId = -1;
+  for (var i = 0, len = dict.length; i < len; i++) {
+    if (lastId == dict[i].id)
+      continue;
 
-// Fill the output_list with the first occurence of each lemmatize word
-var last_id = -1;
-for (var i = 0, len = dict.length; i < len; i++){
-        if (last_id == dict[i].id)
-            continue;
+    outputList.push(dict[i].lemma);
+    lastId = dict[i].id
+  }
 
-        output_list.push(dict[i].lemma);
-        last_id = dict[i].id
-    }
+  return outputList;
+}
 
-console.log(output_list)
+var input = "Bonjour, ceci est un des tests. Au revoir";
+lemmatizeList = lemmatizeInput(input);
+
+const fs = require('fs');
+
+var wordList = fs.readFileSync('data/word-lists.txt').toString().split('\n');
+console.log(wordList);
