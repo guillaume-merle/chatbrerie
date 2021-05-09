479cec851c8897667b154770a1ea3e0d4b32440f
Marc Demoustier <marc.demoustier@epita.fr>
2021-05-10 00:03:14 +0200

    predict the response and log it

--- model/lemmatizer.js
+++ model/lemmatizer.js
@@ -26,10 +26,61 @@ function lemmatizeInput(input) {
   return outputList;
 }
 
-var input = "Bonjour, ceci est un des tests. Au revoir";
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+var input = "Au revoir";
 lemmatizeList = lemmatizeInput(input);
 
 const fs = require('fs');
 
+// get all the words in the file with all the word we used for the learning into an array
 var wordList = fs.readFileSync('data/word-lists.txt').toString().split('\n');
-console.log(wordList);
+
+// create an array full of 0 for the prediction
+var predictInput = new Array(wordList.length).fill(0);
+
+// If a word in the input string is similar to one in the word list, we put a 1 at the position of this word to do the
+// prediction
+
+// TODO: levenstein distance
+for (var i = 0; i < lemmatizeList.length; i++) {
+  for (var j = 0; j < wordList.length; j++) {
+    if (lemmatizeList[i].localeCompare(wordList[j]) == 0) {
+      predictInput[j] = 1;
+    }
+  }
+}
+
+// parse the json file
+var jsonFile = JSON.parse(fs.readFileSync('data/simple.json').toString());
+
+function indexOfMax(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return maxIndex;
+}
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
+
+(async function(){
+  const model = await tf.loadLayersModel('file://data/js-model/model.json');
+
+  var prediction = indexOfMax(Array.from(model.predict(tf.tensor([predictInput])).dataSync()))
+
+  var numberOfResponse = jsonFile['intents'][prediction]['responses'].length;
+  console.log(jsonFile['intents'][prediction]['responses'][getRandomInt(numberOfResponse - 1)]);
+})()
