78662bdac3cd22db8b68fff2b176ba84eb44857c
Guillaume Merle <guillaume.mrle@gmail.com>
2021-05-19 15:21:21 +0200

    Update webpack config and extension manifest

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/chat.js',
+    entry: './src/views/chat.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
