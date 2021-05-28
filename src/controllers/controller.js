9f36a06d9418ad58b6dd87269fcda0b667543dc9
Alexandre DEISS <alexandre.deiss@epita.fr>
2021-05-28 19:45:24 +0200

    add botAnswer function in controller and change the constructor

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,7 +1,19 @@
+import prepareInput from 'lemmatizer.js'
+import Response from '../models/response.js'
+import Model from '../models/model.js'
+import View from '../views/view.js'
+
 class Controller {
-    constructor(view, model){
-        this.view = view
-        this.model = model
+    constructor(){
+        this.view = new View()
+        this.model = new Model()
+        this.response = new Response()
     }
 
+    async botAnswer(input) {
+        var preparedInput = await prepareInput(input)
+        var prediction = this.model.predict(input)
+
+        return this.response.getResponse(prediction)
+    }
 }
