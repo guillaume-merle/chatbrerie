fac05e7713684ef30bdc54d8af9db17d6ec51c1f
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2021-05-29 16:55:12 +0200

    add lemmatizer, use lemmatizer class and export

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,19 +1,22 @@
-import prepareInput from 'lemmatizer.js'
+import Lemmatizer from './lemmatizer.js'
 import Response from '../models/response.js'
 import Model from '../models/model.js'
 import View from '../views/view.js'
 
 class Controller {
     constructor(){
-        this.view = new View()
         this.model = new Model()
         this.response = new Response()
+        this.lemmatizer = new Lemmatizer()
+        this.view = new View(this)
     }
 
     async botAnswer(input) {
-        var preparedInput = await prepareInput(input)
-        var prediction = this.model.predict(input)
+        var preparedInput = await this.lemmatizer.prepareInput(input)
+        var prediction = this.model.predict(preparedInput)
 
         return this.response.getResponse(prediction)
     }
 }
+
+export { Controller }
