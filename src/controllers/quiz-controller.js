a2e69919933cf099f1fdd7973b403803d485fbc9
Guillaume Merle <guillaume.mrle@gmail.com>
2021-06-18 15:58:13 +0200

    Quiz: explanation and launch next question / end quiz

--- src/controllers/quiz-controller.js
+++ src/controllers/quiz-controller.js
@@ -19,15 +19,27 @@ class QuizController {
     callback(event) {
         var validResponse = this.currentQuestion.responses[this.currentQuestion.valid]
 
+        var message = null
+
         if (event.target.id == validResponse.id) {
             event.target.style.backgroundColor = Config.colorValid
+            message = 'Bien joué ! '
         } else {
             event.target.style.backgroundColor = Config.colorWrong
             document.getElementById(validResponse.id).style.backgroundColor = Config.colorValid
+            message = 'Dommage ! '
         }
+
+        this.view.insertMessage(message + this.currentQuestion.explanation, 'bot')
 
         this.#unsetCallbacks()
         this.currentQuestion = this.questionIt.next().value
+
+        if (this.currentQuestion) {
+            this.view.insertQuizQuestion(this.currentQuestion).then(() => this.#setCallbacks())
+        } else {
+            this.view.insertMessage('Le quiz est terminé, merci de votre participation !', 'bot')
+        }
     }
 
     #generateResponseIds() {
