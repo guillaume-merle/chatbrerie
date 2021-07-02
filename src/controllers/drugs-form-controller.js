7d1dbc86c1875fc172d64ac3b83318a8b1bb2089
Marc Demoustier <marc.demoustier@epita.fr>
2021-07-02 19:31:27 +0200

    add class for drugs form controller

---
+++ src/controllers/drugs-form-controller.js
@@ -0,0 +1,10 @@
+import { loadFile, generateId } from '../utils/utils.js'
+import { Config } from '../config'
+
+class DrugsFormController {
+    constructor(view) {
+        this.view = view
+
+    }
+
+export { DrugsFormController }
