5059d1da3d14df7f34ac7f348d4b8b2853c798d2
Marc Demoustier <marc.demoustier@epita.fr>
2021-07-12 00:53:46 +0200

    add documentation for quiz controller

--- src/controllers/quiz-controller.js
+++ src/controllers/quiz-controller.js
@@ -1,8 +1,13 @@
 import { loadFile, generateId, sleep } from '../utils/utils.js'
 import { Config } from '../config'
 
-
+/** Class making a quiz engine to show many quiz for the user */
 class QuizController {
+    /**
+     * Insert the right quiz (thanks to the quizTag) in the current view
+     * @param {View} view - the current chatbot view
+     * @param {string} quizTag - use to find the right json file corresponding to this quiz
+     */
     constructor(view, quizTag) {
         this.view = view
         this.quizPath = [Config.quizBasePath, quizTag + '.json'].join('/')
@@ -22,6 +27,11 @@ class QuizController {
         })
     }
 
+    /**
+     * Callback called when the user press one of the proposition of answer, check if the answer is valid, load the
+     * next question or end the quiz and compute the score
+     * @param {event} event
+     */
     async callback(event) {
         // unset callbacks to not let the user clicks on the buttons again
         this.#unsetCallbacks()
@@ -29,6 +39,7 @@ class QuizController {
         var validResponse = this.currentQuestion.responses[this.currentQuestion.valid]
         var message = null
 
+        // check if the answer is valid
         if (event.target.id == validResponse.id) {
             event.target.style.backgroundColor = Config.colorValid
             message = 'Bien joué ! '
@@ -51,22 +62,30 @@ class QuizController {
             return
         }
 
+        // go to the next question if there is one, or end the quiz and compute score
         this.currentQuestion = this.questionIt.next().value
         if (this.currentQuestion) {
             this.view.insertQuizQuestion(this.currentQuestion).then(() => this.#setCallbacks())
         } else {
             var finalScore = Math.round(this.score / this.quiz.length * 100)
-            this.view.insertMessage('Le quiz est terminé, votre score est de ' + finalScore + '%.\nMerci de votre participation !', 'bot')
+            this.view.insertMessage('Le quiz est terminé, votre score est de ' + finalScore
+                + '%.\nMerci de votre participation !', 'bot')
             this.view.unsetQuizMode()
         }
     }
 
+    /**
+     * Exit the quiz and unset callbacks
+     */
     exit() {
         this.stop = true
         this.#unsetCallbacks()
         this.view.unsetQuizMode()
     }
 
+    /**
+     * Generate a response unique id for a button
+     */
     #generateResponseIds() {
         for (const question of this.quiz) {
             for (const response of question.responses) {
@@ -75,12 +94,18 @@ class QuizController {
         }
     }
 
+    /**
+     * Set callbacks for all the proposition buttons
+     */
     #setCallbacks() {
         for (const response of this.currentQuestion.responses) {
             document.getElementById(response.id).onclick = (event) => this.callback(event) // allows to retain instance context
         }
     }
 
+    /**
+     * Unset callbacks of all buttons
+     */
     #unsetCallbacks() {
         for (const response of this.currentQuestion.responses) {
             document.getElementById(response.id).onclick = null
